08592d8e016e6693c71b5037046e04bd7f9e845b
Jayant Varma <jayant@cellcast.in>
2012-04-25 11:48:15 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mykey::Application.config.secret_token = 'cd08ab36cacd4118b0844cc6cf626376d315ec0de6493b5aef143d9377cb231f84e76e85e3a7d61e8e5db19ef724ac935c37b432ca9c6ae784f9d1c2e30f1eeb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mykey::Application.config.session_store :cookie_store, key: '_mykey_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mykey::Application.config.session_store :active_record_store
